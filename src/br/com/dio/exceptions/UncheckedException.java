613600e790be4ab8fa9cf515b7830274f0f3315d
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2022-12-02 11:36:55 -0300

    sobre as unchecked exception

--- src/br/com/dio/exceptions/UncheckedException.java
+++ src/br/com/dio/exceptions/UncheckedException.java
@@ -6,26 +6,26 @@ import javax.swing.*;
 public class UncheckedException {
     public static void main(String[] args) {
 
-        boolean continueLooping = true;
-        do {
-            String a = JOptionPane.showInputDialog("Numerador: ");
+        boolean continueLooping = true; //iniciando a variável...
+        do {// fazer laço pra que quando dê a exception o código retorne pra pedir os números e o usuário faça o input correto
+            String a = JOptionPane.showInputDialog("Numerador: ");//pra usar esse método show ele retorna uma string, porém quandoo eu for fazer a conta eu vou mudar o tipo e colocar int no caso
             String b = JOptionPane.showInputDialog("Denominador: ");
 
-            try{
-                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-                System.out.println("Resultado: " + resultado);
+            try{ //botamos o try onde pode ocorrer a exception
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));//parseInt pra transf a string em int
+                System.out.println("Resultado: " + resultado); // chegando até aqui n precisa fazer o loop, dando certo o resultado, daí vai direto p o finally
                 continueLooping = false;
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro! " + e.getMessage());
+            } catch (NumberFormatException e) {// quando boto o mouse abaixo do sysout ele pergunta se quero botar o catch, clico e faz automático
+                e.printStackTrace(); // o "e" é um objeto e herda os métodos desse tipo de exception
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro! " + e.getMessage());// nem sempre é bom esse tipo de msg pq palavras em inglês ou conceitos n sabidos podem assustar o usuário
             } catch (ArithmeticException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(null, "Impossível dividir um número por 0.");
             }
-            finally {
+            finally { //é opcional. Dando exception ou n executa o finally // daria pra ter feito o tratamento de excessão pra quando é um divisão que n gera um inteiro
                 System.out.println("Chegou no finally!");
             }
-        } while(continueLooping);
+        } while(continueLooping);//flag
 
 
         System.out.println("O código continua...");
