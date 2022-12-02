f4727e7283025c22893d435ad081479bcf864056
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2022-12-02 12:42:29 -0300

    learning checked exceptions

--- src/br/com/dio/exceptions/CheckedException.java
+++ src/br/com/dio/exceptions/CheckedException.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 import java.io.*;
 
 //Imprimir um arquivo no console.
-public class CheckedException {
+public class CheckedException {//
     public static void main(String[] args) {
         String nomeDoArquivo = "romance-blake-crouch.txt";
 
         try {
-            imprimirArquivoNoConsole(nomeDoArquivo);
+            imprimirArquivoNoConsole(nomeDoArquivo);//botar o cursor em cima do método e gerar o exception. se botarmos o nome errado da erro do read file
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null,
                     "Revise o nome do arquivo que você deseja imprimir! " + e.getCause());
@@ -26,19 +26,19 @@ public class CheckedException {
     }
 
     public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
-        File file = new File(nomeDoArquivo);
+        File file = new File(nomeDoArquivo); //inicializou um obj do tipo file
 
-        BufferedReader br = new BufferedReader(new FileReader(file.getName()));
-        String line = br.readLine();
+        BufferedReader br = new BufferedReader(new FileReader(file.getName()));//pra criar um buff com td o doc q quero imprimir. Só consigo rodar o file reader se eu tratar essa exception, por isso é uma checked exception, para gerar botar o cursor em cima do método e add
+        String line = br.readLine();//lê linha por linha // aqui tbm lança exception. Tanto esse como os abaixo lançam o msm tipo de Exception, clico em cima e troco e dá certo pro reader pq essa nova gerada é mãe daquele
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // para imprimir no console
 
-        do{
-            bw.write(line);
-            bw.newLine();
+        do{ //cria laço pra ler cada linha e imprimir no console
+            bw.write(line); // aqui tbm
+            bw.newLine();// aqui tbm
             line=br.readLine();
         } while(line != null);
-        bw.flush();
-        br.close();
+        bw.flush();//como ta armazenado num buffer vou pedir pra descarregar através do método flush. aqui tbm lança exception
+        br.close();// aqui tbm. se tento rodar sem fazer os exceptions ele indica
     }
 }
